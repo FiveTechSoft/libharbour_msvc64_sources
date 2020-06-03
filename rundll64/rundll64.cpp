e43e9acd71669993406be07f59fa7980653394ca
Antonio Linares <antonio.fivetech@gmail.com>
2020-06-03 13:08:43 +0200

    more

--- rundll64/rundll64.cpp
+++ rundll64/rundll64.cpp
@@ -1,27 +1,34 @@
-// rundll64.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <Windows.h>
-#include <iostream>
+#include <stdio.h>
+
+DWORD WINAPI process( LPVOID );
 
 int main()
 {
-    HMODULE hDll = LoadLibrary( L"libharbour_msvc64_sources.dll" );
+   SECURITY_ATTRIBUTES sa;
+   HANDLE hThread = NULL;
+   DWORD dwThreadId = 0;
+   int i = 0;
 
-    std::cout << "Hello World!\n";
+   ZeroMemory( &sa, sizeof( sa ) );
+   sa.nLength = sizeof( sa );
+
+   while( i++ < 6 )
+   {
+      hThread = CreateThread( &sa, 0, ( LPTHREAD_START_ROUTINE ) process, 0, 0, &dwThreadId ); 
+      // CloseHandle( hThread );
+   }
+
+   return 0;
+}
+
+DWORD WINAPI process( LPVOID )
+{
+    HMODULE hDll = LoadLibrary( "libharbour_msvc64_sources.dll" );
+
+    printf( "\nHello from the EXE" );
 
     FreeLibrary( hDll );
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
